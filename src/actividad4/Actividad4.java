59f3387558cc4d7d85d818d1eef2c363d0cac59a
diegojimdel47 <diego.jimenez.del@gmail.com>
2026-02-13 12:29:58 -0600

    Creación de clases y sus atributos

--- src/actividad4/Actividad4.java
+++ src/actividad4/Actividad4.java
@@ -1,5 +1,41 @@
+package actividad4;
+import java.util.Scanner;
+
 public class Actividad4 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+
     }
 }
+
+    class Subject {
+
+        private String subjectName;
+        private String subjectID;
+        private int creditsNumber;
+        private int weeklyHours;
+    }
+
+    class Teacher {
+
+        private String teacherName;
+        private String payrollNumber;
+        private int wagePerHour;
+        private string assignedSubject;
+    }
+
+    class Course {
+
+        private String courseName;
+        private Subject s1, s2, s3;
+        private int totalCredits;
+    }
+
+    class Student {
+        
+        private String studentID;
+        private String studentName;
+        private int age;
+        private String enrolledSubject;
+    }
